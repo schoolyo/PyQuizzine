faa3cffca70cf4b10637a7cfd73031de2d3bdca2
057019 <057019@comalisd.org>
2023-01-19 10:14:33 -0600

    yeah

---
+++ main.py
@@ -0,0 +1,31 @@
+#import create, loading, visualizing
+
+while True:
+    try:
+        choice = int(input("""What action would you like to perform?:
+            1) Loading
+            2) Creating
+            3) Visualizing
+            """))
+        if (choice == 1):
+            # Loading code here
+            pass
+        elif (choice == 2):
+            # Creating code here
+            pass
+        elif (choice == 3):
+            # Visualizing code here
+            pass
+        break
+    except:
+        print("You need a valid integer")
+
+if (choice == 1):
+    # Loading code here
+    pass
+elif (choice == 2):
+    # Creating code here
+    pass
+elif (choice == 3):
+    # Visualizing code here
+    pass
