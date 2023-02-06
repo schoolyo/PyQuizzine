624f19463dafdbc912a8860dc1d725f2e8dcb99d
057019 <057019@comalisd.org>
2023-02-06 10:29:48 -0600

    does stuff

--- loading.py
+++ loading.py
@@ -4,6 +4,11 @@ import time
 tree = ET.parse('example_quiz.xml')
 root = tree.getroot()
 
+for child in root:
+    if child.attrib == {'type': 'multichoice'}:
+        print("ok")
+        p = tree.find("name")
+        print(p)
 print(root[1][1][0].text)
 userAnswer = input()
 if (userAnswer.lower() == root[1][10][0].text.lower()):
