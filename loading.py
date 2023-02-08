429a1269ffc3a45bf06b3a0055af1ed0be2a051f
057019 <057019@comalisd.org>
2023-02-08 09:46:27 -0600

    lol idk

--- loading.py
+++ loading.py
@@ -3,17 +3,20 @@ import xml.etree.ElementTree as ET
 import time
 tree = ET.parse('example_quiz.xml')
 root = tree.getroot()
-
+ET.tostring(root, encoding='utf8').decode('utf8')
 for child in root:
-    if child.attrib == {'type': 'multichoice'}:
-        print("ok")
-        p = tree.find("name")
-        print(p)
-print(root[1][1][0].text)
+    print(child.tag, child.attrib)
+
+for name in root.iter('question'):
+    print(question.attrib)
+
+print([elem.tag for elem in root.iter()])
+
+"""print(root[1][1][0].text)
 userAnswer = input()
 if (userAnswer.lower() == root[1][10][0].text.lower()):
     print("Correct!")
 else:
     print("Incorrect")
-
+"""
 
