e3adaf78b025ce44366175694d46b1b1ab4783fb
057019 <057019@comalisd.org>
2023-02-13 09:57:23 -0600

    works

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-
+from create import exportXML, makeQuestions
 while True:
     try:
         choice = int(input("""What action would you like to perform?:
@@ -14,6 +14,7 @@ if choice == 1:
     import loading
 
 elif choice == 2:
-    import create
+    quizName = input("Input the name of your quiz")
+    exportXML(makeQuestions(), quizName)
 else:
     import visual
