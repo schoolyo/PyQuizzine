bcbf64de81b0b74354cbfb641adeefe4014bb443
057019 <057019@comalisd.org>
2023-02-21 10:30:30 -0600

    uhyeah

--- main.py
+++ main.py
@@ -1,4 +1,25 @@
 from create import exportXML, makeQuestions
+import tkinter as tk
+class App(tk.Frame):
+    def __init__(self, master=None):
+        super().__init__(master)
+        self.pack()
+
+# create the application
+myapp = App()
+
+#
+# here are method calls to the window manager class
+#
+myapp.master.title("PyQuizzine")
+myapp.master.maxsize(1000, 400)
+
+tk.Button(frm, text="Quit", command=root.destroy).grid(column=1, row=0)
+
+
+# start the program
+myapp.mainloop()
+
 while True:
     try:
         choice = int(input("""What action would you like to perform?:
@@ -14,7 +35,7 @@ if choice == 1:
     import loading
 
 elif choice == 2:
-    quizName = input("Input the name of your quiz")
+    quizName = input("Input the name of your quiz: ")
     exportXML(makeQuestions(), quizName)
 else:
     import visual
