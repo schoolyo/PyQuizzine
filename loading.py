1e779bbb318a12446d32fc20d2052ac684eda2fc
057019 <057019@comalisd.org>
2023-03-06 10:23:51 -0600

    why did we do this to ourselves

--- loading.py
+++ loading.py
@@ -1,30 +1,44 @@
 import xml.etree.ElementTree as ET
 import time
+import csv
+from tkinter import *
+from tkinter import ttk
 tree = ET.parse(input("Input XML file name of quiz: ").strip() + ".xml")
 root = tree.getroot()
+questCount = 0
+name = input("What is your name?: ")
 
-# Goal: test efficiency of algorithm on other quiz xml files
-# Figure out how to print certain questions for each
+with open('data.csv', 'w', newline='') as csvfile:
+    fieldnames = ['student', 'quiz time', 'percent score']
+    writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
+
+    writer.writeheader()
+    writer.writerow({'student': name})
+
+
+# create CSV file, student name, quiz time, percent, 1 or 0 for questions correct
+
+
 count = 1
 score = 0
 questions = []
 answers = []
-name = input("What is your name?: ")
+booleanCorrects = []
 # Gets all questions in xml file
 
 for questiontext in root.iter('questiontext'):
     questions.append(questiontext[0].text.strip())
 
-print(questions)
 # Gets all correct answers for corresponding questions in xml file
 
 
 
 for answer in root.iter('answer'):
     answers.append(answer[0].text.strip())
-print(answers)
+    questCount += 1
 
-
+for i in range(0, questCount):
+    fieldnames.append('q' + str(i))
 
 print("This quiz is scored 0 out of " + str(len(answers) - 1) + "\n")
 
@@ -35,15 +49,21 @@ for k in range(0, len(questions)):
 
     print(questions[k])
     for i in range(0, len(answers)):
-        if i % 4 != 0 or i == 0:
+        if i % 4 != 0 or i == 0: # Iterate through four answers 
             print(str(count) + ")" + " " + answers[i])
             count += 1
+            booleanCorrects.append("0")
     userAns = input()
     
     for i in range(0, len(answers)):
         if userAns.lower() == answers[i].lower():
             score += 1
+            booleanCorrects.append("1")
             print("\nCorrect!")
+        else:
+            booleanCorrects.append("0")
 
 print("\nYour score is " + str(score))
-print("You took " + str(round(time.time() - start))  + " seconds")
+time = time.time() - start
+print("You took " + str(round(time))  + " seconds")
+
