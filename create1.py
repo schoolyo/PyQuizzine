492f756fab85dfd5bcf3606673c59884957510e9
pityviper <youngp6475@g.comalisd.org>
2023-03-07 23:22:26 -0600

    so close to the end of suffering, yet so far

---
+++ create1.py
@@ -0,0 +1,129 @@
+from quizpy import Quiz, Category, MultipleChoice, ShortAnswer, Numerical
+import os
+import re
+
+def makeQuestions():
+    """
+    Creates a Quiz object and adds questions to it using user input
+
+    :return: a Quiz object q
+    :type q: Quiz
+    """
+    count = 1
+    q = Quiz()
+    qName = input("What is the name of the quiz?: ")
+    qCategory = q.add_category(qName)
+    while True: # loop for creating all questions
+        question = input("What is the question?:")
+        points = float(input("How many points for getting this question correct?: "))
+        clearScreen()
+        while True: # loop for creating a single question
+            try:
+                qType = int(input("""What kind of question do you want to make?: 
+                1) Multiple Choice
+                2) Numerical
+                3) Short Answer\n"""))
+            except:
+                raise ValueError("input does not meet the conditions")
+            clearScreen()
+
+            if 1 <= qType <= 3:
+                if qType == 1: # create Multiple Choice question & append to category
+                    mc = MultipleChoice(f"q{count}", question, points)
+                    makeMC(mc)
+                    qCategory.questions.append(mc)
+                    clearScreen()
+                    count += 1
+                    break
+                elif qType == 2: # create Numerical question & append to category
+                    num = Numerical(f"q{count}", question, points)
+                    answer = float(input("What is the correct numerical answer?: "))
+                    num.add_answer(answer, 100.0, "Correct!")
+                    qCategory.questions.append(num)
+                    clearScreen()
+                    count += 1
+                    break
+                elif qType == 3: # create Short Answer question & append to category
+                    sa = ShortAnswer(f"q{count}", question, points)
+                    answer = input("What is the correct answer?: ")
+                    sa.add_answer(answer, 100.0, "Correct!", "plain_text")
+                    qCategory.questions.append(sa)
+                    clearScreen()
+                    count += 1
+                    break
+
+        try:
+            done = int(input("Are you done creating questions?:\n1) Yes\n2) No\n"))
+        except:
+            raise ValueError("Number did not match the input requirements")
+        if 1 <= done <= 2:
+            if done == 1: # done creating questions
+                clearScreen()
+                break
+            elif done == 2: # not done
+                clearScreen()
+    return q
+
+
+def exportXML(qz, fname):
+    """
+    exports Quiz object as XML file
+
+    :param qz: Quiz object that contains the questions and answers
+    :type qz: Quiz
+    :param fname: name for exported file
+    :type fname: str
+    """
+    fname = re.sub(" ", "_", fname.lower())
+    for i in fname:
+        if re.match(r"\W", i):
+            fname = fname.replace(i, "", 1)
+        else:
+            break
+    fname += ".xml"
+    qz.export(fname)
+
+
+def makeMC(mc):
+    """
+    creates a MultipleChoice question object using user input
+
+    :param mc: MultipleChoice object
+    """
+    count = 0
+    while count < 4:
+        choice = input("Enter an answer choice: ")
+        try:
+            correct = int(input("is this the correct answer?\n1) Yes\n2) No\n"))
+        except:
+            raise ValueError("Number did not meet input requirements")
+        clearScreen()
+        if 1 <= correct <= 2:
+            count += 1
+            if correct == 1: # is correct answer
+                mc.add_choice(choice, 100.0, "correct!")
+            elif correct == 2: # isn't correct answer
+                mc.add_choice(choice, 0.0, "incorrect!")
+        try:
+            done = int(input("Are you done inputting answer choices?\n1) Yes\n2) No\n"))
+        except:
+            raise ValueError("Number did not meet the input requirements")
+        if 1 <= done <= 2:
+            if done == 1: # done with answer choices
+                clearScreen()
+                break
+            elif done == 2: # not done with answer choices
+                clearScreen()
+
+
+def clearScreen():
+    """clears the terminal of any text"""
+    if os.name == "posix": # linux or mac
+        os.system("clear")
+    else: # windows
+        os.system("cls")
+
+
+quiz = makeQuestions()
+name = input("What is the filename?")
+exportXML(quiz, name)
--- visualizing.py
+++ visualizing.py
@@ -1,7 +1,9 @@
 import matplotlib.pyplot as plt
-import csv
+from matplotlib.figure import Figure
+from matplotlib.backends.backend_tkagg import (FigureCanvasTkAgg, NavigationToolbar2Tk)
 import pandas as pd
 import tkinter as tk
+from tkinter import font as tkfont
 
 
 class Results:
@@ -32,7 +34,7 @@ class Results:
         Returns:
             A float representing the average time in seconds
         """
-        return sum([i for i in self.data.iloc[:, "time"] if i > 0]) / self.numRows
+        return sum([i for i in self.data.iloc[:, 1] if i > 0]) / self.numRows
 
     def avg_score(self):
         """
@@ -40,7 +42,7 @@ class Results:
         Returns:
             A float representing the average score as a percentage
         """
-        return sum([i for i in self.data.iloc[:, "percent score"]]) / self.numRows
+        return sum([i for i in self.data.iloc[:, 2]]) / self.numRows
 
     def question_stats(self, num):
         """
@@ -51,15 +53,191 @@ class Results:
             A tuple in (percent correct, percent incorrect) format, where both are rounded to 2 decimal places
         """
         correct = round(sum([i for i in self.data.iloc[:, (num+2)] if i == 1]) / self.numRows * 100, 2)
-        incorrect = round(sum([i for i in self.data.iloc[:, (num+2)] if i == 0]) / self.numRows * 100, 2)
-        return correct, incorrect
+        incorrect = 100.0 - correct
+        return [correct, incorrect]
 
 
-"""
-To Do:
-create gui pages
-maybe need to create initialization initializes using a page class, so it can create for questions that vary in number
-figure out how to embed charts in the tkinter frame
-maybe ask Talan about putting the question titles as the column headers in csv file (q1, q2, etc.) since I've already 
-    put in question titles
-"""
+class QuizApp(tk.Tk):
+    """
+    Controller class for frames of the PyQuizzine application
+    ...
+    Attributes:
+        title_font : Font attributes for titles
+        font : Font attributes for regular text
+        frames : array of the page names
+        results : instance of Results class based off of csv file
+    """
+    def __init__(self, csv="csv_quiz_example_format.csv", *args, **kwargs):
+        tk.Tk.__init__(self, *args, **kwargs)
+        self.title_font = tkfont.Font(family="Helvetica", size=18, weight="bold", slant="italic")
+        self.font = tkfont.Font(family="Helvetica", size=11)
+        container = tk.Frame(self)
+        container.pack(side="top", fill="both", expand=True)
+        self.frames = {}
+        self.results = Results(csv_file=csv)
+        for F in (Initialize, StartPage, QuizStats, QuestionStats):
+            page_name = F.__name__
+            frame = F(container, self)
+            self.frames[page_name] = frame
+
+            # put all of the pages in the same location;
+            # the one on the top of the stacking order
+            # will be the one that is visible.
+            frame.grid(row=0, column=0, sticky="nsew")
+
+        self.show_frame("Initialize")
+
+    def show_frame(self, page_name):
+        """Show a frame for the given page name"""
+        frame = self.frames[page_name]
+        frame.tkraise()
+
+
+class Initialize(tk.Frame):
+    """
+    First page of application, asks for the name of the csv file to load
+    """
+    def __init__(self, parent, controller):
+        tk.Frame.__init__(self, parent)
+        self.controller = controller
+
+        def open_file(fname):
+            """
+            Initializes the results class with inputted csv file
+            Arg:
+                fname: a str for the name of the csv file to be read
+            """
+            controller.results = Results(fname)
+            controller.show_frame("StartPage")
+        for i in range(5):
+            self.grid_rowconfigure(i, weight=1)
+            self.grid_columnconfigure(i, weight=1)
+        # Create visual elements
+        label1 = tk.Label(self, text="Load File", font=controller.title_font)
+        l1 = tk.Label(self, text="csv file name", font=controller.font)
+        e1 = tk.Entry(self)
+        button1 = tk.Button(self, text="Open File", font=controller.font,
+                            command=lambda: open_file(e1.get()))
+        # Arrange visual elements
+        label1.grid(row=0, column=1, rowspan=2)
+        l1.grid(row=2, column=1, sticky="E")
+        e1.grid(row=2, column=2, sticky="W")
+        button1.grid(row=3, column=1)
+
+
+class StartPage(tk.Frame):
+    """
+    Main page of the application that has navigation to other pages
+    """
+    def __init__(self, parent, controller):
+        tk.Frame.__init__(self, parent)
+        self.controller = controller
+        for i in range(5):
+            self.grid_rowconfigure(i, weight=1)
+            self.grid_columnconfigure(i, weight=1)
+        # Creating visual elements
+        label1 = tk.Label(self, text="Start Page", font=controller.title_font)
+        button1 = tk.Button(self, text="Quiz Statistics", font=controller.font,
+                            command=lambda: controller.show_frame("QuizStats"))
+        button2 = tk.Button(self, text="Question Statistics", font=controller.font,
+                            command=lambda: controller.show_frame("QuestionStats"))
+        button3 = tk.Button(self, text="Exit", font=controller.font,
+                            command=lambda: controller.destroy())
+        # Arranging visual elements
+        label1.grid(row=0, column=1, rowspan=2)
+        button1.grid(row=2, column=0, sticky="E")
+        button2.grid(row=2, column=2, sticky="W")
+        button3.grid(row=0, column=0, sticky="NW")
+
+
+class QuizStats(tk.Frame):
+    """
+    Application page that displays general statistics about the entire quiz
+    """
+    def __init__(self, parent, controller):
+        tk.Frame.__init__(self, parent)
+        self.controller = controller
+        for i in range(5):
+            self.grid_rowconfigure(i, weight=1)
+            self.grid_columnconfigure(i, weight=1)
+        # Creating visual elements
+        label1 = tk.Label(self, text="Quiz Statistics", font=controller.title_font)
+        l1 = tk.Label(self, text=f"Average time: {controller.results.avg_time() / 60:.2f} minutes", font=controller.font)
+        l2 = tk.Label(self, text=f"Average score: {controller.results.avg_score():.2f}%", font=controller.font)
+        button1 = tk.Button(self, text="Start Page", font=controller.font,
+                            command=lambda: controller.show_frame("StartPage"))
+        # Arranging visual elements
+        label1.grid(row=0, column=1, rowspan=2)
+        l1.grid(row=2, column=1)
+        l2.grid(row=3, column=1)
+        button1.grid(row=0, column=0, sticky="NW")
+
+
+class QuestionStats(tk.Frame):
+    """
+    Application page that displays a graph for statistics of a user-specified question
+    ...
+    Attributes:
+        canvas : tkinter Canvas object that contains the graph
+        stats : tuple of the (correct, incorrect) percentages on the specified question
+    """
+    def __init__(self, parent, controller):
+        tk.Frame.__init__(self, parent)
+        self.controller = controller
+        self.canvas = None
+
+        def ex():
+            """
+            Destroys the chart Canvas when exiting the page
+            """
+            try:
+                self.canvas.get_tk_widget().delete("all")
+                self.canvas.get_tk_widget().destroy()
+            except AttributeError:
+                pass
+            controller.show_frame("StartPage")
+
+        def show_stats(num):
+            """
+            Creates a pie chart, if doesn't already exist, of the stats for the user-inputted question number
+            Args:
+                num: an int that represents the question number to display data for
+            """
+            if self.canvas is None:
+                self.stats = controller.results.question_stats(int(num))
+                fig = Figure(figsize=(5, 5), dpi=100)
+                fig, ax = plt.subplots()
+                ax.pie([self.stats[0], self.stats[1]], labels=["Correct", "Incorrect"], colors=["blue", "red"])
+                print(self.stats)
+                self.canvas = FigureCanvasTkAgg(fig, master=controller)
+                self.canvas.draw()
+                self.canvas.get_tk_widget().pack()
+                self.canvas.get_tk_widget().pack()
+            else:
+                self.canvas.get_tk_widget().delete("all")
+                self.canvas.get_tk_widget().destroy()
+                self.canvas = None
+                show_stats(num)
+        for i in range(5):
+            self.grid_rowconfigure(i, weight=1)
+            self.grid_columnconfigure(i, weight=1)
+        # Create visual elements
+        label1 = tk.Label(self, text="Question Statistics", font=controller.title_font)
+        l1 = tk.Label(self, text="Question Number", font=controller.font)
+        e1 = tk.Entry(self)
+        button1 = tk.Button(self, text="View Stats", font=controller.font,
+                            command=lambda: show_stats(e1.get()))
+        button2 = tk.Button(self, text="Start Page", font=controller.font,
+                            command=lambda: ex())
+        # Arrange visual elements
+        label1.grid(row=0, column=1, rowspan=2)
+        l1.grid(row=3, column=1, sticky="E")
+        e1.grid(row=3, column=2, sticky="W")
+        button1.grid(row=4, column=0)
+        button2.grid(row=0, column=0, sticky="NW")
+
+
+app = QuizApp(className="PyQuizzine", csv="csv_quiz_example_format.csv")
+app.geometry("500x500")
+app.mainloop()
+
